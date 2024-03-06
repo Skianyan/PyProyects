415a8da327b8b5219e7b72a47ba70e347427ed4b
Ricardo Haro <99053383+Skianyan@users.noreply.github.com>
2024-03-06 12:14:47 -0800

    ccom

--- 3dCanvas.py
+++ 3dCanvas.py
@@ -1,4 +1,4 @@
-import graphlib as gf
+import gfxlib as gf
 from math import floor
 from PIL import Image, ImageDraw
 
@@ -15,33 +15,62 @@ from PIL import Image, ImageDraw
 
 width = 501
 height = 501
+
 ##############################################################################################
 
 # Definir un lienzo
 canvas = Image.new('RGB', (int(width), int(height)), (255, 255, 255))
 
 # Definir Vertices frontales
-vAf = gf.Vertex(-200, -50.5, 50)
-vBf = gf.Vertex(-200, -50.5, 50)
-vCf = gf.Vertex(-100, -50.5, 50)
-vDf = gf.Vertex(-100, -50.5, 50)
+vAf = gf.Vertex(2, -0.5, 5)
+vBf = gf.Vertex(2,  0.5, 5)
+vCf = gf.Vertex(1,  0.5, 5)
+vDf = gf.Vertex(1, -0.5, 5)
 
 # Definir Vertices posteriores
-vAb = gf.Vertex(-200, -50.5, 60)
-vBb = gf.Vertex(-200, -50.5, 60)
-vCb = gf.Vertex(-100, -50.5, 60)
-vDb = gf.Vertex(-100, -50.5, 60)
+vAb = gf.Vertex(2, -0.5, 6)
+vBb = gf.Vertex(2,  0.5, 6)
+vCb = gf.Vertex(1,  0.5, 6)
+vDb = gf.Vertex(1, -0.5, 6)
+
+# Juntar los vertices en una lista
+vertices = []
+vertices.append(vAf),vertices.append(vBf),vertices.append(vCf),vertices.append(vDf)
+vertices.append(vAb),vertices.append(vBb),vertices.append(vCb),vertices.append(vDb),
 
 # Definir los colores
-BLUE = (10, 10, 255)
+BLUE = (10,10,255)
+RED = (255,10,10)
+GREEN = (10,255,10)
+YELLOW = (238,232,170)
+PURPLE = (186,85,211)
+CYAN = (0,255,255)
 
+# Definir los triangulos
+triangles = [(0,1,2,RED),(0,2,3,RED),(4,0,3,GREEN),(4,3,7,GREEN),(5,4,7,BLUE),(5,7,6,BLUE),
+             (1,5,6,YELLOW),(1,6,2,YELLOW),(4,5,1,PURPLE),(4,1,0,PURPLE),(2,6,7,CYAN),(2,7,3,CYAN)]
+
+# Graficar los puntos
+gf.renderObject(vertices,triangles,canvas)
 ##############################################################################################
 
 # Trazar Caras
 
-gf.drawLine(gf.proyectVertex(vAf, canvas), gf.proyectVertex(vBf, canvas), BLUE, canvas)
-gf.drawLine(gf.proyectVertex(vBf, canvas), gf.proyectVertex(vCf, canvas), BLUE, canvas)
-gf.drawLine(gf.proyectVertex(vCf, canvas), gf.proyectVertex(vDf, canvas), BLUE, canvas)
-gf.drawLine(gf.proyectVertex(vDf, canvas), gf.proyectVertex(vAf, canvas), BLUE, canvas)
+
+
+# gf.drawLine(gf.projectVertex(vAf,canvas), gf.projectVertex(vBf,canvas), BLUE,canvas)
+# gf.drawLine(gf.projectVertex(vBf,canvas), gf.projectVertex(vCf,canvas), BLUE,canvas)
+# gf.drawLine(gf.projectVertex(vCf,canvas), gf.projectVertex(vDf,canvas), BLUE,canvas)
+# gf.drawLine(gf.projectVertex(vDf,canvas), gf.projectVertex(vAf,canvas), BLUE,canvas)
+#
+# gf.drawLine(gf.projectVertex(vAb,canvas), gf.projectVertex(vBb,canvas), RED, canvas)
+# gf.drawLine(gf.projectVertex(vBb,canvas), gf.projectVertex(vCb,canvas), RED, canvas)
+# gf.drawLine(gf.projectVertex(vCb,canvas), gf.projectVertex(vDb,canvas), RED, canvas)
+# gf.drawLine(gf.projectVertex(vDb,canvas), gf.projectVertex(vAb,canvas), RED, canvas)
+#
+# gf.drawLine(gf.projectVertex(vAf,canvas), gf.projectVertex(vAb,canvas), GREEN, canvas)
+# gf.drawLine(gf.projectVertex(vBf,canvas), gf.projectVertex(vBb,canvas), GREEN, canvas)
+# gf.drawLine(gf.projectVertex(vCf,canvas), gf.projectVertex(vCb,canvas), GREEN, canvas)
+# gf.drawLine(gf.projectVertex(vDf,canvas), gf.projectVertex(vDb,canvas), GREEN, canvas)
 
 canvas.show()
--- gfxlib.py
+++ gfxlib.py
@@ -1,308 +1,193 @@
 import math
-from PIL import Image, ImageDraw
-
-import math
-import tkinter as tk
-import keyboard
-from tkinter import *
-from tkinter import colorchooser
-from PIL import Image, ImageTk, ImageColor
+import numpy as np
 
 
+# A Point.
 class Pt:
-    def __init__(self, x, y, h=1):
-        self.x = x
-        self.y = y
-        self.h = h
+	def __init__(self, x, y, h=1):
+            self.x = x
+            self.y = y
+            self.h = h
 
-##########################################
-# Drawing Functions
+# A 3D vertex.
+class Vertex:
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+
+# The putPixel() function.
+def putPixel(x, y, color, canvas):
+	x = canvas.width/2 + x
+	y = canvas.height/2 - y
+
+	if (x < 0 or x >= canvas.width or y < 0 or y >= canvas.height):
+		return
+	canvas.putpixel((int(x), int(y)), color)
 
 
-def putPixel(x, y, color):
-    if is_rgb(color):
-        color = rgb_to_hex(color[0],color[1],color[2])
-    canvas.create_rectangle(x, y, x + 1, y + 1, outline=color)
+def interpolate(i0, d0, i1, d1):
+	if (i0 == i1):
+		return [d0]
+
+	values = []
+	a = (d1 - d0) / (i1 - i0)
+	d = d0
+	for i in range(i0, i1+1):
+		values.append(d)
+		d += a
+
+	return values
 
 
-def drawShadedTriangle(p0, p1, p2, fill):
-    if is_rgb(fill) == FALSE:
-        fill = ImageColor.getcolor(fill, "RGB")
+def drawLine(p0, p1, color, canvas):
+	dx = p1.x - p0.x
+	dy = p1.y - p0.y
 
-    # Define correctamente los puntos
-    if p1.y < p0.y:
-        p0, p1 = p1, p0
-    if p2.y < p0.y:
-        p0, p2 = p2, p0
-    if p2.y < p1.y:
-        p1, p2 = p2, p1
+	if (abs(dx) > abs(dy)):
+		# The line is horizontal-ish. Make sure it's left to right.
+		if (dx < 0):
+			p0, p1 = p1, p0
 
-    # Definir los valores de sombreado
-    h0 = p0.h
-    h1 = p1.h
-    h2 = p2.h
+		# Compute the Y values and draw.
+		ys = interpolate(p0.x, p0.y, p1.x, p1.y)
+		for x in range(p0.x, p1.x):
+			putPixel(x, ys[(x - p0.x) | 0], color, canvas)
 
-    # Definir puntos para legibilidad
-    x0 = p0.x
-    y0 = p0.y
-    x1 = p1.x
-    y1 = p1.y
-    x2 = p2.x
-    y2 = p2.y
+	else:
+		# The line is verical-ish. Make sure it's bottom to top.
+		if (dy < 0):
+			p0, p1 = p1, p0
 
-    x01 = interpolate(y0, x0, y1, x1)
-    h01 = interpolate(y0, h0, y1, h1)
-    x02 = interpolate(y0, x0, y2, x2)
-    h02 = interpolate(y0, h0, y2, h2)
-    x12 = interpolate(y1, x1, y2, x2)
-    h12 = interpolate(y1, h1, y2, h2)
+		# Compute the X values and draw.
+		xs = interpolate(p0.y, p0.x, p1.y, p1.x)
+		for y in range(p0.y, p1.y):
+			putPixel(xs[(y - p0.y) | 0], y, color, canvas)
 
-    #print('x01 =', x01)
-    #print('x02 =', x02)
-    #print('x12 =', x12)
+def drawWireframeTriangle (P0, P1, P2, color, canvas):
+    drawLine(P0, P1, color, canvas)
+    drawLine(P1, P2, color, canvas)
+    drawLine(P2, P0, color, canvas)
 
-    # Concatenate Short sides 'x'
+def drawFilledTriangle (P0, P1, P2, color,canvas):
+    # Sort the points so that y0 <= y1 <= y2
+
+    if P1.y < P0.y:
+        P0, P1 = P1, P0
+    if P2.y < P0.y:
+        P0, P2 = P2, P0
+    if P2.y < P1.y:
+        P1, P2 = P2, P1
+
+    # Compute the x coordinates of the triangle edges
+    x01 = interpolate(P0.y, P0.x, P1.y, P1.x)
+    x12 = interpolate(P1.y, P1.x, P2.y, P2.x)
+    x02 = interpolate(P0.y, P0.x, P2.y, P2.x)
+
+    #Concatenate the short sides
+    #remove_last(x01)
     x01.pop(-1)
     x012 = x01 + x12
 
-    # Concatenate short sides 'h'
+    #Determine which is left and which is right
+    m = math.floor(len(x012) / 2)
+    if x02[m] < x012[m]:
+        x_left = x02
+        x_right = x012
+    else:
+        x_left = x012
+        x_right = x02
+
+    # Draw the horizontal segments
+    for y in range(P0.y,P2.y+1):
+        a=round(x_left[y - P0.y])
+        b=round(x_right[y - P0.y])
+        for x in range(a,b):
+            putPixel(x,y,color,canvas)
+
+def drawShadedTriangle (P0, P1, P2, color,canvas):
+    # Sort the points so that y0 <= y1 <= y2
+
+    if P1.y < P0.y:
+        P0, P1 = P1, P0
+    if P2.y < P0.y:
+        P0, P2 = P2, P0
+    if P2.y < P1.y:
+        P1, P2 = P2, P1
+
+    # Compute the x coordinates of the triangle edges
+    x01 = interpolate(P0.y, P0.x, P1.y, P1.x)
+    h01 = interpolate(P0.y, P0.h, P1.y, P1.h)
+
+    x12 = interpolate(P1.y, P1.x, P2.y, P2.x)
+    h12 = interpolate(P1.y, P1.h, P2.y, P2.h)
+
+    x02 = interpolate(P0.y, P0.x, P2.y, P2.x)
+    h02 = interpolate(P0.y, P0.h, P2.y, P2.h)
+
+    #Concatenate the short sides
+    #remove_last(x01)
+    x01.pop(-1)
+    x012 = x01 + x12
+
     h01.pop(-1)
     h012 = h01 + h12
 
-    #print('x012 =', x012)
-    #print(x012.__len__())
-    #print(x02.__len__())
-
+    #Determine which is left and which is right
     m = math.floor(len(x012) / 2)
     if x02[m] < x012[m]:
-        xleft = x02
-        hleft = h02
+        x_left = x02
+        h_left = h02
 
-        xright = x012
-        hright = h012
+        x_right = x012
+        h_right = h012
     else:
-        xleft = x012
-        hleft = h012
+        x_left = x012
+        h_left = h012
 
-        xright = x02
-        hright = h02
+        x_right = x02
+        h_right = h02
 
-    for y in range(y0, y2 + 1):
-        xl = round(xleft[y - y0])
-        xr = round(xright[y - y0])
-        h_seg = interpolate(xl, hleft[y - y0], xr, hright[y - y0])
+    # Draw the horizontal segments
+    for y in range(P0.y,P2.y+1):
+        xl=round(x_left[y - P0.y])
+        hl=h_left[y - P0.y]
 
-        for x in range(xl, xr):
-            shadedColor0 = int(fill[0] * h_seg[x - xl])
-            shadedColor1 = int(fill[1] * h_seg[x - xl])
-            shadedColor2 = int(fill[2] * h_seg[x - xl])
-            shadedColor = (shadedColor0, shadedColor1, shadedColor2)
-            putPixel(x, y, shadedColor)
+        xr=round(x_right[y - P0.y])
+        hr=h_right[y - P0.y]
 
+        h_segment = interpolate(xl,hl,xr,hr)
 
-def drawShadedPolygon(points, fill):
-    fill = ImageColor.getcolor(fill, "RGB")
-    p_cent = calcular_centroide(points)
-    #print("centroide (x,y):", p_cent.x, p_cent.y, p_cent.h)
-    n = len(points)
-    #print("Cuantos puntos:", n)
+        for x in range(xl,xr):
+            sh_color0 = round(color[0] * h_segment[x - xl])
+            sh_color1 = round(color[1] * h_segment[x - xl])
+            sh_color2 = round(color[2] * h_segment[x - xl])
+            shaded_color = (sh_color0,sh_color1,sh_color2)
 
-    for current_point, next_point in zip(points, points[1:]):
-        #print("cp", current_point.x, current_point.y, current_point.h)
-        drawShadedTriangle(current_point, next_point, p_cent, fill)
-        #drawLine(current_point, next_point, "#000")
+            putPixel(x,y,shaded_color,canvas)
 
-    if next_point == points[n - 1]:
-        #print("cp", current_point.x, current_point.y, current_point.h)
-        drawShadedTriangle(points[0], next_point, p_cent, fill)
-        #drawLine(points[0], next_point, "#000")
+def projectVertex(V, canvas):
+    projection_plane_z = 1
+    xp = V.x * projection_plane_z / V.z
+    yp = V.y * projection_plane_z / V.z
+    Pp = Pt(xp, yp)
+    return viewportToCanvas(Pp, canvas)
 
-    return
+def viewportToCanvas(P2d, canvas):
+    viewport_size = 1
+    return Pt(int(P2d.x * canvas.width / viewport_size) | 0, int(P2d.y * canvas.height / viewport_size) | 0)
 
-
-def drawLine(p0, p1, color):
-    x0 = p0.x
-    y0 = p0.y
-    x1 = p1.x
-    y1 = p1.y
-
-    if abs(x1 - x0) > abs(y1 - y0):
-        # Es una linea horizontal
-        if x0 > x1:
-            x0, x1, y0, y1 = x1, x0, y1, y0
-        ys = interpolate(x0, y0, x1, y1)
-        for x in range(x0, x1):
-            putPixel(int(x), int(ys[x-x0]), color)
-    else:
-        # Es una linea vertical
-        if y0 > y1:
-            x0, x1, y0, y1 = x1, x0, y1, y0
-
-        xs = interpolate(y0, x0, y1, x1)
-
-        for y in range(y0, y1):
-            putPixel(int(xs[y-y0]), int(y), color);
-
-##########################################
-# Calculation Functions
+def renderObject(vertices, triangles, canvas):
+    projected = []
+    for V in vertices:
+        projected.append(projectVertex(V,canvas))
+    for T in triangles:
+        renderTriangle(T, projected, canvas)
 
 
-def rgb_to_hex(r, g, b):
-    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
+def renderTriangle(triangle, projected, canvas):
+    drawFilledTriangle(projected[triangle[0]],
+                          projected[triangle[1]],
+                          projected[triangle[2]],
+                          triangle[3], canvas)
 
-
-def is_rgb(color):
-    if not isinstance(color, tuple):
-        return False
-    if len(color) != 3:
-        return False
-    for component in color:
-        if not isinstance(component, int):
-            return False
-        if component < 0 or component > 255:
-            return False
-    return True
-
-
-def calcular_centroide(puntos):
-    n = len(puntos)
-
-    sum_x = sum(point.x for point in puntos)
-    sum_y = sum(point.y for point in puntos)
-    sum_h = sum(point.h for point in puntos)
-
-    centroide_x = int(sum_x / n)
-    centroide_y = int(sum_y / n)
-    centroide_h = sum_h / n
-    cent = Pt(centroide_x, centroide_y, centroide_h)
-    return cent
-
-
-def interpolate (i0,f0,i1,f1):
-    if i0 == i1:
-        return [f0]
-    values = []
-    a = (f1-f0)/(i1-i0)
-    f = f0
-    for i in range(i0, i1 + 1):
-        values.append(f)
-        f = f + a
-    return values
-
-
-def getIntValue(value, min, max):
-    try:
-        value = int(input("Define " + value + ": "))
-    except ValueError:
-        print('Enter a valid integer')
-    if value > max:
-        print("Please enter a number smaller than " + str(max))
-    if value < min:
-        print("Please enter a number larger than " + str(min))
-
-        exit()
-    return value
-
-
-##########################################
-# UI Functions
-# Function to track cursor clicks on canvas
-
-
-def on_click(event):
-    if points.__len__() < 7:
-        x, y = event.x, event.y
-        #Place pixel (Placeholder)
-        putPixel(x, y, color)
-        #Add click position to points
-        points.append(Pt(x, y))
-        print("Clicked at:" + " X: " + str(x) + " Y: " + str(y))
-        generate_slider()
-    else:
-        print("max 7 points")
-
-
-def on_enter(event):
-    if event.name == 'enter' and color is not None:
-        sliderval = get_slider_values()
-        print("sliderval 'premod'", sliderval)
-        sliderval = [x/100 for x in sliderval]
-        print("sliderval", sliderval)
-        update_h_values(points, sliderval)
-        # Draws a line last 2 points clicked
-        # if points.__len__() > 1:
-        #     drawLine(points[-1], points[-2], "#333")
-
-        # Centroide...
-        # cent = calcular_centroide(points)
-        # print(cent.x, cent.y )
-
-        #print(points[-3].x)
-        #print(points[-2].x)
-        #print(points[-1].x)
-
-        # Draws a Triangle between all the points
-        #drawPointTriangle(points[-3], points[-2], points[-1], fill)
-
-        # Draws a polygon between all the points
-        # testpuntos =[Pt(100, 100, 1), Pt(100, 200, .9), Pt(200, 200, .6), Pt(200, 100, .4)]
-        print(points.__len__())
-        if points.__len__() == 2:
-            drawLine(points[-2], points[-1], color)
-            reset_params()
-        if points.__len__() == 3:
-            drawShadedTriangle(points[-3], points[-2], points[-1], color)
-            reset_params()
-        if points.__len__() > 3:
-            drawShadedPolygon(points, color)
-            reset_params()
-        print("enter / action done")
-        #print(ImageColor.getcolor("#800080", "RGB"))
-    else:
-        if event.name == 'enter' and color is not None:
-            print("please select a color")
-
-
-def clear_canvas():
-    canvas.delete('all')
-    canvas.create_image(0, 0, anchor=NW, image=cv)
-    points.clear()
-    delete_sliders(frame_sliders)
-
-def generate_slider():
-    new_slider = tk.Scale(frame_sliders, from_=0, to=100, orient=tk.HORIZONTAL)
-    new_slider.pack(padx=10, pady=5)
-    sliders.append(new_slider)
-
-def delete_sliders(frame_sliders):
-    for slider in sliders:
-        slider.destroy()
-    sliders.clear()
-
-def select_color():
-    global color
-    color = show_color_palette()
-    if color:
-        color_label.config(bg=color)
-    print(color)
-
-def reset_params():
-    points.clear()
-    delete_sliders(frame_sliders)
-    sliders = []
-def show_color_palette():
-    color = colorchooser.askcolor()[1]
-    return color
-
-def update_h_values(points_array, sliders):
-    for point, slider_value in zip(points_array, sliders):
-        point.h = slider_value
-def get_slider_values():
-    slider_values = []
-    for i, slider in enumerate(sliders):
-        value = slider.get()
-        print(f"Slider {i + 1} value:", value)
-        slider_values.append(value)
-    return slider_values
-
-##########################################
